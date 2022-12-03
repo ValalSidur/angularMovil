cfea31881f44c83d2b512dd199cd544747a4af58
Alejandro Valenzuela Silerio <alexer@tec.mx>
2022-12-02 22:15:01 -0600

    Semana 4
    Se agregan pruebas con Karma se explora circle y appium

--- my-drawer-ng/src/app/browse/browse.component.ts
+++ my-drawer-ng/src/app/browse/browse.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core'
 import { RadSideDrawer } from 'nativescript-ui-sidedrawer'
 import { Application } from '@nativescript/core'
+import { registerElement } from '@nativescript/angular'
+
+registerElement("MapView", () => require("nativescript-google-maps-sdk").MapView);
 
 @Component({
   selector: 'Browse',
   templateUrl: './browse.component.html',
 })
 export class BrowseComponent implements OnInit {
+  @ViewChild("MapView") mapView: ElementRef;
+
   constructor() {
     // Use the component constructor to inject providers.
   }
@@ -18,5 +23,9 @@ export class BrowseComponent implements OnInit {
   onDrawerButtonTap(): void {
     const sideDrawer = <RadSideDrawer>Application.getRootView()
     sideDrawer.showDrawer()
+  }
+
+  onMarReady(event): void {
+    console.log("Map ready");
   }
 }
--- my-drawer-ng/src/app/home/home.component.ts
+++ my-drawer-ng/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core'
 import { RadSideDrawer } from 'nativescript-ui-sidedrawer'
-import { Application } from '@nativescript/core'
+import { Application, ImageAsset } from '@nativescript/core'
+import * as camera from "nativescript-camera";
+
 
 @Component({
   selector: 'Home',
@@ -18,5 +20,24 @@ export class HomeComponent implements OnInit {
   onDrawerButtonTap(): void {
     const sideDrawer = <RadSideDrawer>Application.getRootView()
     sideDrawer.showDrawer()
+  }
+
+  onButtonTap(): void{
+    camera.requestPermissions().then(
+      function success(){
+        const options = { width: 300, height: 300, keepAspectRatio: false, saveToGallery: true };
+        camera.takePicture(options).
+          then((ImageAsset) => {
+            console.log("Tamaño: "+ImageAsset.options.width+"x"+ImageAsset.options.height);
+            console.log("keepAspectRatio: "+ImageAsset.options.keepAspectRatio);
+            console.log("Foto guardada!");
+          }).catch((err) => {
+            console.log("error -> "+err.mesage);
+          });
+      },
+      function failure(){
+        console.log("Permiso de camara no aceptado por el usuario");
+      }
+    )
   }
 }
